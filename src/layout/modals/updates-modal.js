1c9d600e7ab6eeb64127ef18cea466b4becb7462
Manuel Scholz <manuel.serrano943@gmail.com>
2025-09-30 18:08:54 +0200

    Feat: Updates modal

---
+++ src/layout/modals/updates-modal.js
@@ -0,0 +1,62 @@
+import { useContext, useEffect, useState } from 'react';
+import { Modal, View, Text, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ui } from '../utils/styles';
+import Button from '../components/button';
+import { LangContext } from '../../utils/LangContext';
+import { APP, userPreferences } from '../../utils/user-preferences';
+
+
+export default function UpdatesModal() {
+    const [visible, setVisible] = useState(false);
+    const { language } = useContext(LangContext);
+
+
+    useEffect(() => {
+        const checkIfSeen = async () => {
+            const version = await AsyncStorage.getItem(userPreferences.CHANGELOG_VERSION);
+            if (version !== APP.currentVersion) {
+                setVisible(true);
+            }
+        };
+        checkIfSeen();
+    }, []);
+
+    const closeModal = async () => {
+        setVisible(false);
+        await AsyncStorage.setItem(userPreferences.CHANGELOG_VERSION, APP.currentVersion);
+    };
+
+    return (
+        <Modal visible={visible} transparent animationType="slide">
+            <View style={styles.overlay}>
+                <View style={styles.modal}>
+                    <Text style={[ui.h3, { color: "#000" }]}>🆕 {language.t("_updatesModalNews")}</Text>
+                    <Text style={[ui.text, { color: "#000" }]}>• {language.t("_updatesModalV1_1")}</Text>
+                    <Text style={[ui.text, { color: "#000" }]}>• {language.t("_updatesModalV1_2")}</Text>
+                    <Text style={[ui.text, { color: "#000" }]}>• {language.t("_updatesModalV1_3")}</Text>
+                    <Text style={[ui.text, { color: "#000" }]}>• {language.t("_updatesModalV1_4")}</Text>
+                    <Text style={[ui.text, { color: "#000" }]}>• {language.t("_updatesModalV1_5")}</Text>
+                    <Text style={[ui.text, { color: "#000" }]}>• {language.t("_updatesModalV1_6")}</Text>
+                    <Button text={"Cerrar"} onClick={closeModal} />
+                </View>
+            </View>
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modal: {
+        backgroundColor: '#fff',
+        padding: 24,
+        borderRadius: 8,
+        width: '80%',
+        gap: 16
+    },
+});
---
+++ src/utils/user-preferences.js
@@ -0,0 +1,6 @@
+export const userPreferences = {
+    CHANGELOG_VERSION: "changelog_version",
+}
+export const APP = {
+    currentVersion: "2.0.0"
+}
